2cd6021bbefdb1d88f8e975119665ec4e46f55e8
Kozachok Sergiy <senjust@gmail.com>
2018-10-04 14:28:50 +0300

    created skeleton of InternetHospitalUI

--- InternetHospitalUI/src/app/app.module.ts
+++ InternetHospitalUI/src/app/app.module.ts
@@ -2,16 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MaterialModule } from './Modules/material/material.module';
+import { HeaderComponent } from './Components/Layout/header/header.component';
+import { SignInComponent } from './Components/Authorization/sign-in/sign-in.component';
+import { SignUpComponent } from './Components/Authorization/sign-up/sign-up.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SignInComponent,
+    SignUpComponent,
   ],
   imports: [
     BrowserModule,
-    MatToolbarModule
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
