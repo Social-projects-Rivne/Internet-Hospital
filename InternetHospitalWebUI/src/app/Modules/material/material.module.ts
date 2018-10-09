c78d4b14eb5cc0c02d0855958433c493bf6a8eb8
Kozachok Sergiy <senjust@gmail.com>
2018-10-09 17:57:46 +0300

    Created angular-skeleton

---
+++ InternetHospitalWebUI/src/app/Modules/material/material.module.ts
@@ -0,0 +1,51 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule, MatIconModule, MatDialogModule, MatNativeDateModule } from '@angular/material';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatCardModule } from '@angular/material/card';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatGridListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatCheckboxModule,
+    MatNativeDateModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    MatIconModule,
+    MatDialogModule,
+    MatCardModule
+  ],
+  declarations: [],
+  exports: [
+    MatToolbarModule,
+    MatGridListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatCheckboxModule,
+    MatNativeDateModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    MatIconModule,
+    MatDialogModule,
+    MatCardModule
+  ]
+})
+export class MaterialModule { }
