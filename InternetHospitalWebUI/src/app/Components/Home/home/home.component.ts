45e43b51fae6ac2f0c20fa298823a2931ce9ffd2
Kozachok Sergiy <senjust@gmail.com>
2018-10-11 08:34:49 +0300

    Added titles to news

--- InternetHospitalWebUI/src/app/Components/Home/home/home.component.ts
+++ InternetHospitalWebUI/src/app/Components/Home/home/home.component.ts
@@ -8,12 +8,12 @@ import { HomeImages } from '../../../Models/Temp/HomeImage';
 })
 export class HomeComponent implements OnInit {
   homeImages: HomeImages[] = [
-    {id: 1, url: 'https://cdn.vortala.com/static/uploads/8/files/2017/10/midland-park-dental.jpg'},
-    {id: 2, url: 'https://cdn.vortala.com/static/uploads/8/files/2018/06/trust-dental-group.jpg'},
-    {id: 3, url: 'https://cdn.vortala.com/static/uploads/8/files/2018/06/seaport-dental.jpg'},
-    {id: 4, url: 'https://cdn.vortala.com/static/uploads/8/files/2018/06/red-hill-dental.jpg'},
-    {id: 5, url: 'https://cdn.vortala.com/static/uploads/8/files/2018/06/focus-orthodontics.jpg'},
-    {id: 6, url: 'https://cdn.vortala.com/static/uploads/8/files/2018/06/green-apple-dental.jpg'},
+    {id: 1, title: 'New Jersey Favorite Dentist', url: 'https://cdn.vortala.com/static/uploads/8/files/2017/10/midland-park-dental.jpg'},
+    {id: 2, title: 'Trust dental group', url: 'https://cdn.vortala.com/static/uploads/8/files/2018/06/trust-dental-group.jpg'},
+    {id: 3, title: 'Sea Part dental', url: 'https://cdn.vortala.com/static/uploads/8/files/2018/06/seaport-dental.jpg'},
+    {id: 4, title: 'Red Hill Dental', url: 'https://cdn.vortala.com/static/uploads/8/files/2018/06/red-hill-dental.jpg'},
+    {id: 5, title: 'FocusOrthodontics', url: 'https://cdn.vortala.com/static/uploads/8/files/2018/06/focus-orthodontics.jpg'},
+    {id: 6, title: 'Green Apple Dental Clinic', url: 'https://cdn.vortala.com/static/uploads/8/files/2018/06/green-apple-dental.jpg'},
   ]
 
   constructor() { }
