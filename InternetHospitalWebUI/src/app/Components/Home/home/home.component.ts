64430cccd35e169a672576a98f3f52ae99ad628c
Kozachok Sergiy <senjust@gmail.com>
2018-10-11 22:00:15 +0300

    Transported const HOME_IMAGES from home-component to mock-file

--- InternetHospitalWebUI/src/app/Components/Home/home/home.component.ts
+++ InternetHospitalWebUI/src/app/Components/Home/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HomeImages } from '../../../Models/Temp/HomeImage';
+import { HOME_IMAGES } from '../../../Mock-Objects/mock-home-news';
 
 @Component({
   selector: 'app-home',
@@ -7,14 +7,7 @@ import { HomeImages } from '../../../Models/Temp/HomeImage';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  homeImages: HomeImages[] = [
-    {id: 1, title: 'New Jersey Favorite Dentist', url: 'https://cdn.vortala.com/static/uploads/8/files/2017/10/midland-park-dental.jpg'},
-    {id: 2, title: 'Trust dental group', url: 'https://cdn.vortala.com/static/uploads/8/files/2018/06/trust-dental-group.jpg'},
-    {id: 3, title: 'Sea Part dental', url: 'https://cdn.vortala.com/static/uploads/8/files/2018/06/seaport-dental.jpg'},
-    {id: 4, title: 'Red Hill Dental', url: 'https://cdn.vortala.com/static/uploads/8/files/2018/06/red-hill-dental.jpg'},
-    {id: 5, title: 'FocusOrthodontics', url: 'https://cdn.vortala.com/static/uploads/8/files/2018/06/focus-orthodontics.jpg'},
-    {id: 6, title: 'Green Apple Dental Clinic', url: 'https://cdn.vortala.com/static/uploads/8/files/2018/06/green-apple-dental.jpg'},
-  ]
+  homeImages = HOME_IMAGES;
 
   constructor() { }
 
