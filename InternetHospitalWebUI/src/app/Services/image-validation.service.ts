70f42a775c50e1f11005b8d451211fb050d68f48
Andrii Voznovych <korrag96@gmail.com>
2018-10-16 19:03:48 +0300

    Changed private values to const

--- InternetHospitalWebUI/src/app/Services/image-validation.service.ts
+++ InternetHospitalWebUI/src/app/Services/image-validation.service.ts
@@ -5,6 +5,9 @@ enum TypeCodes{
   PNG="89504e470d0a1a0a",
   JPG="ffd8ff",
 }
+const BASE64_MARKER:string = ';base64,';
+const AMOUNT_OF_BYTES_FOR_CHECK = 16;
+const SYSTEM_OF_NUMBERS = 16;
 
 @Injectable({
   providedIn: 'root'
@@ -13,12 +16,10 @@ export class ImageValidationService {
 
   constructor(private http: HttpClient) { }
 
-  private BASE64_MARKER:string = ';base64,';
-  private AMOUNT_OF_BYTES_FOR_CHECK = 16;
-  private SYSTEM_OF_NUMBERS = 16;
+
 
   private convertDataURIToBinary(dataURI): Uint8Array {  
-    var base64Index = dataURI.indexOf(this.BASE64_MARKER) + this.BASE64_MARKER.length;
+    var base64Index = dataURI.indexOf(BASE64_MARKER) + BASE64_MARKER.length;
     var base64 = dataURI.substring(base64Index);
     var raw = window.atob(base64);
     var array = new Uint8Array(new ArrayBuffer(raw.length));
@@ -31,14 +32,14 @@ export class ImageValidationService {
   private getImageHeader(img: Uint8Array): string {
     var header: string = "";
     for(var i = 0; i < img.length; i++) {
-       header += img[i].toString(this.SYSTEM_OF_NUMBERS);
+       header += img[i].toString(SYSTEM_OF_NUMBERS);
     }
     return header;
   }
 
   isImageFile(uri): boolean {
     var isImg = false;
-    var arr = this.convertDataURIToBinary(uri).subarray(0, this.AMOUNT_OF_BYTES_FOR_CHECK);
+    var arr = this.convertDataURIToBinary(uri).subarray(0, AMOUNT_OF_BYTES_FOR_CHECK);
     var header = this.getImageHeader(arr);
     if(header.slice(0, TypeCodes.JPG.length) === TypeCodes.JPG
         || header.slice(0, TypeCodes.PNG.length) === TypeCodes.PNG){
