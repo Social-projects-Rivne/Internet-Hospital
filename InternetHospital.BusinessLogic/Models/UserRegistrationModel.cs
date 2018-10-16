36ae292e9a48656be3ff0973ad89586af0c8236a
edward0802 <edik.kucherniuk@gmail.com>
2018-10-16 13:49:40 +0300

    Edited validation in UserRegistrationModel.cs

--- InternetHospital.BusinessLogic/Models/UserRegistrationModel.cs
+++ InternetHospital.BusinessLogic/Models/UserRegistrationModel.cs
@@ -8,16 +8,17 @@ namespace InternetHospital.BusinessLogic.Models
         [Required]
         public string UserName { get; set; }
         [Required]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
         [Required]
         public string Role { get; set; }
         [Required]
         [DataType(DataType.Password)]
+        [RegularExpression(@"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.*\s).{8,24}$", ErrorMessage = "Password must be at least 8 characters and contain digits, upper and lower case and non alphanumeric symbols")]
         public string Password { get; set; }
         [Required]
         [DataType(DataType.Password)]
         [Compare("Password", ErrorMessage = "Passwords are different")]
-        [RegularExpression(@"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.*\s).{8,24}$", ErrorMessage = "Password must be at least 8 characters and contain digits, upper and lower case and non alphanumeric symbols")]
         public string ConfirmPassword { get; set; }
     }
 }
