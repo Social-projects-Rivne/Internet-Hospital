79d4e4945fff6ffd2d9b4b58f9bd831d8fecdf00
Martyniuk Roman <hjbfrcgq@gmail.com>
2018-10-22 23:34:30 +0300

    routing

--- InternetHospitalWebUI/src/app/Modules/routing/routing.module.ts
+++ InternetHospitalWebUI/src/app/Modules/routing/routing.module.ts
@@ -5,10 +5,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { SignUpComponent } from '../../Components/Authorization/sign-up/sign-up.component';
 import { SignInComponent } from '../../Components/Authorization/sign-in/sign-in.component';
 import { HomeComponent } from '../../Components/Home/home/home.component';
+import { Page404Component } from '../../Components/page404/page404.component'
+
 const ROUTES: Routes = [
   { path: '', component: HomeComponent },
   { path: 'sign-up', component: SignUpComponent },
-  { path: 'sign-in', component: SignInComponent } 
+  { path: 'sign-in', component: SignInComponent },
+  { path: '404', component: Page404Component },
+
+  { path: '**', redirectTo: '404' },
 ];
 
 @NgModule({
@@ -25,4 +30,5 @@ export const ROUTING_COMPONENTS = [
   SignUpComponent,
   SignInComponent,
   HomeComponent,
+  Page404Component,
 ]
