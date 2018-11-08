33904d04a1505e7afd9d8305919e869e03e588f9
Oleksandr Antoniuk <oanto@softserveinc.com>
2018-11-08 13:31:37 +0200

    add first unit test for appointment service

---
+++ InternetHospital.UnitTests/Appointments/AppointmentServiceUnitTests.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AutoFixture;
+using FluentAssertions;
+using Xunit;
+using InternetHospital.BusinessLogic.Models;
+using InternetHospital.BusinessLogic.Services;
+using InternetHospital.DataAccess;
+using InternetHospital.DataAccess.Entities;
+using InternetHospital.UnitTests.TestHelpers;
+
+namespace InternetHospital.UnitTests.Appointments
+{
+    /// <summary>
+    /// Represents unit tests for AppointmentService.
+    /// </summary>
+    public class AppointmentServiceUnitTests
+    {
+        [Theory]
+        [InlineData(1)]
+        public void ShouldGetMyAppointments(int doctorId)
+        {
+            // arrange
+            const int ALLOWED_STATUS_ID = 1;
+
+            // get in memory db options
+            var options = DbContextHelper.GetDbOptions(nameof(ShouldGetMyAppointments));
+            
+            // create fixture 
+            var fixture = FixtureHelper.CreateOmitOnRecursionFixture();
+
+            // create test appointment for test case
+            var fixtureAppointment = fixture.Build<Appointment>()
+                                            .With(a => a.DoctorId, doctorId)
+                                            .With(a => a.StatusId, ALLOWED_STATUS_ID)
+                                            .With(a => a.Status, 
+                                                  new AppointmentStatus
+                                                  {
+                                                      Id = ALLOWED_STATUS_ID
+                                                  })
+                                            .With(a => a.UserId, 1)
+                                            .With(a => a.User, 
+                                                  new User
+                                                  {
+                                                      Id = 1
+                                                  })
+                                            .Create();
+
+            var expectedData = GetExpectedAppointments(fixtureAppointment);
+
+            // add data to in memory database
+            using (var context = new ApplicationContext(options))
+            {
+                context.AddRange(fixtureAppointment);
+                context.SaveChanges();
+            }
+
+            // use a clean instance of the context to run the test
+            using (var context = new ApplicationContext(options))
+            {
+                // create service with generated data
+                var appointmentService = new AppointmentService(context);
+
+                // act
+                var appointments = appointmentService.GetMyAppointments(doctorId)
+                                                     .ToList();
+
+                // assert
+                appointments.Should().BeEquivalentTo(expectedData);
+            }
+        }
+
+        private List<AppointmentModel> GetExpectedAppointments(Appointment apoinment)
+        {
+            var model = new AppointmentModel
+                        {
+                            Id = apoinment.Id,
+                            UserId = apoinment.UserId,
+                            UserFirstName = apoinment.User.FirstName,
+                            UserSecondName = apoinment.User.SecondName,
+                            Address = apoinment.Address,
+                            StartTime = apoinment.StartTime,
+                            EndTime = apoinment.EndTime,
+                            Status = apoinment.Status.Name
+                        };
+
+            return new List<AppointmentModel> { model };
+        }
+    }
+}
---
+++ InternetHospital.UnitTests/TestHelpers/FixtureHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using AutoFixture;
+
+namespace InternetHospital.UnitTests.TestHelpers
+{
+    public static class FixtureHelper
+    {
+        /// <summary>
+        /// Creates OmitOnRecursionBehavior as opposite to ThrowingRecursionBehavior.
+        /// </summary>
+        /// <returns>Fixture instance.</returns>
+        public static Fixture CreateOmitOnRecursionFixture()
+        { 
+            //from https://github.com/AutoFixture/AutoFixture/issues/337
+            var fixture = new Fixture();
+
+            fixture.Behaviors
+                   .OfType<ThrowingRecursionBehavior>()
+                   .ToList()
+                   .ForEach(b => fixture.Behaviors.Remove(b));
+
+            //recursionDepth
+            fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+
+            return fixture;
+        }
+    }
+}
