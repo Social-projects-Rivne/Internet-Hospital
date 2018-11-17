29e681896a37ad21c8b1fadc079136e256011b2e
Andriy Slesarchuk <slesarchukandr@gmail.com>
2018-11-17 13:09:51 +0200

    Add IllnessHistory entity

--- InternetHospital.DataAccess/ApplicationContext.cs
+++ InternetHospital.DataAccess/ApplicationContext.cs
@@ -17,6 +17,9 @@ namespace InternetHospital.DataAccess
         public DbSet<Status> Statuses { get; set; }
         public DbSet<FeedBack> FeedBacks { set; get; }
         public DbSet<FeedBackType> FeedBackTypes { set; get; }
+        public DbSet<IllnessHistory> IllnessHistories { set; get; }
+        public DbSet<Passport> Passports { set; get; }
+        public DbSet<TemporaryUser> TemporaryUsers { set; get; }
 
         public ApplicationContext(DbContextOptions<ApplicationContext> options)
            : base(options)
--- InternetHospital.DataAccess/Entities/IllnessHistory.cs
+++ InternetHospital.DataAccess/Entities/IllnessHistory.cs
@@ -4,17 +4,17 @@ using System.Text;
 
 namespace InternetHospital.DataAccess.Entities
 {
-    public class Illness
+    public class IllnessHistory
     {
         public int Id { get; set; }
-        public int UserId { get; set; }
+        public int? UserId { get; set; }
         public int DoctorId { get; set; }
         public string Symptoms { get; set; }
         public string Diagnose { get; set; }
         public string Treatment { get; set; }
         public DateTime ConclusionTime { get; set; }
 
-        public User User { get; set; }
-        public Doctor Doctor { get; set; }
+        public virtual User User { get; set; }
+        public virtual Doctor Doctor { get; set; }
     }
 }
