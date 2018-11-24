5878deea5bde8cb4c59147fa8fe6a4ce72006357
Andriy Slesarchuk <slesarchukandr@gmail.com>
2018-11-25 00:27:29 +0200

    Update illness history entity

--- InternetHospital.DataAccess/Entities/IllnessHistory.cs
+++ InternetHospital.DataAccess/Entities/IllnessHistory.cs
@@ -9,12 +9,19 @@ namespace InternetHospital.DataAccess.Entities
         public int Id { get; set; }
         public int? UserId { get; set; }
         public int DoctorId { get; set; }
-        public string Symptoms { get; set; }
+        public int? AppointmentId { get; set; }
+        public string Complaints { get; set; }
+        public string DiseaseAnamnesis { get; set; }
+        public string LifeAnamnesis { get; set; }
+        public string ObjectiveStatus { get; set; }
+        public string LocalStatus { get; set; }
         public string Diagnose { get; set; }
-        public string Treatment { get; set; }
+        public string SurveyPlan { get; set; }
+        public string TreatmentPlan { get; set; }
         public DateTime ConclusionTime { get; set; }
 
         public virtual User User { get; set; }
         public virtual Doctor Doctor { get; set; }
+        public virtual Appointment Appointment { get; set; }
     }
 }
