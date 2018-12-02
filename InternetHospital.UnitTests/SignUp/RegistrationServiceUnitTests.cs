c5524bee3cd8798e8cbab59fa7dbea1b5a8e4927
edward0802 <edik.kucherniuk@gmail.com>
2018-12-02 16:01:47 +0200

    Added unit tests for registration service and signup service

---
+++ InternetHospital.UnitTests/SignUp/RegistrationServiceUnitTests.cs
@@ -0,0 +1,81 @@
+﻿using AutoFixture;
+using AutoMapper;
+using InternetHospital.BusinessLogic.Models;
+using InternetHospital.BusinessLogic.Services;
+using InternetHospital.DataAccess;
+using InternetHospital.DataAccess.Entities;
+using InternetHospital.UnitTests.TestHelpers;
+using Xunit;
+
+namespace InternetHospital.UnitTests.SignUp
+{
+    public class RegistrationServiceUnitTests
+    {
+        public RegistrationServiceUnitTests()
+        {
+            Mapper.Reset();
+            Mapper.Initialize(cfg => cfg.CreateMap<UserRegistrationModel, User>());
+        }
+
+        [Fact]
+        public async void ShouldPatientRegistrate()
+        {
+            const string PATIENT = "Patient";
+
+            var options = DbContextHelper.GetDbOptions(nameof(ShouldPatientRegistrate));
+            var fixture = FixtureHelper.CreateOmitOnRecursionFixture();
+
+            var expectedUser = fixture.Build<User>()
+                .Create();
+
+            var fixtureRegistrationModel = RegistrationHelper.GetRegistrationModel(expectedUser, PATIENT);
+            var fakeUserManager = RegistrationHelper.GetConfiguredUserManager();
+            var fakeRoleManager = RegistrationHelper.GetFakeRoleManager();
+
+            using (var context = new ApplicationContext(options))
+            {
+                var registrationService = new RegistrationService(context, fakeUserManager.Object, fakeRoleManager.Object);
+
+                // act 
+                var res = await registrationService.PatientRegistration(fixtureRegistrationModel);
+
+                // asset
+                Assert.Equal(expectedUser.Email, res.Email);
+            }
+        }
+
+        [Fact]
+        public async void ShoulDoctorRegistrate()
+        {
+            const int USER_ID = 2;
+            const string DOCTOR = "Doctor";
+
+            var options = DbContextHelper.GetDbOptions(nameof(ShoulDoctorRegistrate));
+            var fixture = FixtureHelper.CreateOmitOnRecursionFixture();
+
+            var fixutureUser = fixture.Build<User>()
+                .With(u => u.Id, USER_ID)
+                .With(u => u.Doctor, new Doctor
+                {
+                    UserId = USER_ID
+                })
+                .Create();
+
+            var fixtureRegistrationModel = RegistrationHelper.GetRegistrationModel(fixutureUser, DOCTOR);
+            var fakeUserManager = RegistrationHelper.GetConfiguredUserManager();
+            var fakeRoleManager = RegistrationHelper.GetFakeRoleManager();
+
+            using (var context = new ApplicationContext(options))
+            {
+                var registrationService = new RegistrationService(context, fakeUserManager.Object, fakeRoleManager.Object);
+
+                // act 
+                var res = await registrationService.DoctorRegistration(fixtureRegistrationModel);
+
+                // assert
+                Assert.Equal(res.Email, fixutureUser.Email);
+                Assert.Equal(fixutureUser.Doctor.UserId.ToString(), res.Doctor.UserId.ToString());
+            }
+        }
+    }
+}
---
+++ InternetHospital.UnitTests/TestHelpers/RegistrationHelper.cs
@@ -0,0 +1,112 @@
+﻿using AutoFixture;
+using InternetHospital.BusinessLogic.Models;
+using InternetHospital.DataAccess.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using System;
+using System.IO;
+
+namespace InternetHospital.UnitTests.TestHelpers
+{
+    public static class RegistrationHelper
+    {
+        const int NEW_USER_STATUS = 2;
+
+        public static Mock<UserManager<User>> GetFakeUserManager()
+        {
+            return new Mock<UserManager<User>>(
+                new Mock<IUserStore<User>>().Object,
+                new Mock<IOptions<IdentityOptions>>().Object,
+                new Mock<IPasswordHasher<User>>().Object,
+                new IUserValidator<User>[0],
+                new IPasswordValidator<User>[0],
+                new Mock<ILookupNormalizer>().Object,
+                new Mock<IdentityErrorDescriber>().Object,
+                new Mock<IServiceProvider>().Object,
+                new Mock<ILogger<UserManager<User>>>().Object);
+        }
+
+        public static Mock<UserManager<User>> GetConfiguredUserManager()
+        {
+            var fakeUserManager = GetFakeUserManager();
+            fakeUserManager.Setup(um => um.CreateAsync(It.IsAny<User>(), It.IsAny<string>()))
+                .ReturnsAsync(IdentityResult.Success);
+            fakeUserManager.Setup(um => um.AddToRoleAsync(It.IsAny<User>(), It.IsAny<string>()))
+                .ReturnsAsync(IdentityResult.Success);
+
+            return fakeUserManager;
+        }
+
+        public static Mock<RoleManager<IdentityRole<int>>> GetFakeRoleManager()
+        {
+            return new Mock<RoleManager<IdentityRole<int>>>(
+                    new Mock<IRoleStore<IdentityRole<int>>>().Object,
+                    new IRoleValidator<IdentityRole<int>>[0],
+                    new Mock<ILookupNormalizer>().Object,
+                    new Mock<IdentityErrorDescriber>().Object,
+                    new Mock<ILogger<RoleManager<IdentityRole<int>>>>().Object);
+        }
+
+        public static Mock<IFormFile> GetFakeFile()
+        {
+            var fileMock = new Mock<IFormFile>();
+            var content = "Fake File";
+            var fileName = "test.jpg";
+            var ms = new MemoryStream();
+            var writer = new StreamWriter(ms);
+            writer.Write(content);
+            writer.Flush();
+            ms.Position = 0;
+            fileMock.Setup(_ => _.OpenReadStream()).Returns(ms);
+            fileMock.Setup(_ => _.FileName).Returns(fileName);
+            fileMock.Setup(_ => _.Length).Returns(ms.Length);
+
+            return fileMock;
+        }
+
+        public static User GetNewPatient(Fixture fixture)
+        {
+            return fixture.Build<User>()
+                .With(p => p.StatusId, NEW_USER_STATUS)
+                .With(p => p.Status, new Status
+                {
+                    Id = 2,
+                    Name = "New"
+                })
+                .With(p => p.Doctor, null)
+                .Create();
+        }
+
+        public static User GetNewDoctor(Fixture fixture)
+        {
+            return fixture.Build<User>()
+                .With(u => u.Id, 2)
+                .With(u => u.StatusId, NEW_USER_STATUS)
+                .With(u => u.Status, new Status
+                {
+                    Id = 2,
+                    Name = "New"
+                })
+                .With(u => u.Doctor, new Doctor
+                {
+                    UserId = 2
+                })
+                .Create();
+        }
+
+        public static UserRegistrationModel GetRegistrationModel(User user, string role)
+        {
+            return new UserRegistrationModel
+            {
+                Email = user.Email,
+                UserName = user.UserName,
+                Role = role,
+                Password = "1234Pass",
+                ConfirmPassword = "1234Pass"
+            };
+        }
+    }
+}
